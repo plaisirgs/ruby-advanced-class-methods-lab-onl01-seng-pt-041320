4502e7057f688834c65170669de7c64480ba33f6
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-10 21:44:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,57 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
-  @@all = []
-
+  @@all= []
+  
   def self.all
-    @@all
+    @@all 
   end
 
   def save
     self.class.all << self
   end
 
+  def self.create
+   song = self.new
+   song.save
+   song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song =self.new
+    song.name = name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+    
+  end
+
+  def self.new_from_filename(song_name)
+   song = self.new
+   song.name = name
+   song_name = song.name.chomp(".mp3").split("-")
+end
+  
+  
+  
+  
+  
 end
