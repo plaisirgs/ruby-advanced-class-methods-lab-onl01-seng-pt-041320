7612d45b08916af52935f7649e289260b053190c
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-10 21:48:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
 
   def self.new_from_filename(filename)
    split_filename = filename.chop.chop.chop.chop.split("-")
-   song = song.new
+   song = Song.new
    song.name = split_filename[1]
    song.artist_name = split_filename[0]
    song
